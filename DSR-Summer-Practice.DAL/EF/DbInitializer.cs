c3e1050b192c25f20f898fdd2b95a413175abca0
vitaliy <vitality.boldorev@yandex.ru>
2022-07-10 21:01:13 +0300

    add services, shared, dal

--- DSR-Summer-Practice.DAL/EF/DbInitializer.cs
+++ DSR-Summer-Practice.DAL/EF/DbInitializer.cs
@@ -1,37 +1,53 @@
 ﻿using DSR_Summer_Practice.DAL.Entities;
 using System.Data.Entity;
 using System.Xml;
+using DSR_Summer_Practice.Shared.Interfaces;
+using DSR_Summer_Practice.Shared.Entieties;
 
 namespace DSR_Summer_Practice.DAL.EF
 {
     public class DbInitializer : DropCreateDatabaseIfModelChanges<AppDBContext>
     {
+        private IRepository<ValCurs> repository;
+        public DbInitializer(IRepository<ValCurs> repository)
+        {
+            this.repository = repository;
+        }
         protected override void Seed(AppDBContext context)
         {
-            String URLString = "http://www.cbr.ru/scripts/XML_daily.asp";
-            var currencyNames = new List<string>();
-            XmlDocument xdoc = new XmlDocument();
-            xdoc.Load(URLString);
-            XmlElement? xmlRoot = xdoc.DocumentElement;
-            if (xmlRoot != null)
-            {
-                foreach (XmlElement xnode in xmlRoot)
-                {
-                    foreach (XmlNode xn in xnode.ChildNodes)
-                    {
-                        if (xn.Name == "Name")
-                        {
-                            currencyNames.Add(xn.InnerText);
-                        }
-                    }
-                }
-            }
+            var currencies = repository.get();
+            //String URLString = "http://www.cbr.ru/scripts/XML_daily.asp";
+            //var currencyNames = new List<string>();
+            //XmlDocument xdoc = new XmlDocument();
+            //xdoc.Load(URLString);
+            //XmlElement? xmlRoot = xdoc.DocumentElement;
+            //if (xmlRoot != null)
+            //{
+            //    foreach (XmlElement xnode in xmlRoot)
+            //    {
+            //        foreach (XmlNode xn in xnode.ChildNodes)
+            //        {
+            //            if (xn.Name == "Name")
+            //            {
+            //                currencyNames.Add(xn.InnerText);
+            //            }
+            //        }
+            //    }
+            //}
 
-            foreach (var currency in currencyNames)
+            //foreach (var currency in currencyNames)
+            //{
+            //    context.Currencies.Add(new Currency
+            //    {
+            //        Name = currency
+            //    });
+            //}
+
+            foreach (var currency in currencies.Valute)
             {
                 context.Currencies.Add(new Currency
                 {
-                    Name = currency
+                    Name = currency.Name
                 });
             }
             context.SaveChanges();
